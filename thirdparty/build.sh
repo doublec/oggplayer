e8c66fd437ab701101221c137ca7293b5426ee1c
Chris Double <chris.double@double.co.nz>
2009-03-26 02:02:31 +1300

    Fix broken build.sh

--- thirdparty/build.sh
+++ thirdparty/build.sh
@@ -5,7 +5,7 @@ cd libogg
 make && make install
 cd ../libvorbis
 ./autogen.sh
-./configure --prefix=$PREFIX/local --with-ogg=$PREFIX/local --disable-shared --enable-statuc
+./configure --prefix=$PREFIX/local --with-ogg=$PREFIX/local --disable-shared --enable-static
 make && make install
 cd ../libtheora
 ./autogen.sh
@@ -17,11 +17,11 @@ cd ../liboggz
 make && make install
 cd ../libfishsound
 ./autogen.sh
-OGGZ_CFLAGS=-I$PREFIX/local/include OGGZ_LIBS="-L$PREFIX/lib -loggz" VORBIS_CFLAGS=-I$PREFIX/local/include VORBIS_LIBS="-L$PREFIX/lib -lvorbis" ./configure --prefix=$PREFIX/local --disable-speex --disable-flac --disable-encode --disable-shared --enable-static
+OGGZ_CFLAGS=-I$PREFIX/local/include OGGZ_LIBS="-L$PREFIX/local/lib -loggz" VORBIS_CFLAGS=-I$PREFIX/local/include VORBIS_LIBS="-L$PREFIX/local/lib -lvorbis" ./configure --prefix=$PREFIX/local --disable-speex --disable-flac --disable-encode --disable-shared --enable-static
 make && make install
 cd ../liboggplay
 ./autogen.sh
-OGGZ_CFLAGS=-I$PREFIX/local/include OGGZ_LIBS="-L$PREFIX/lib -loggz" VORBIS_CFLAGS=-I$PREFIX/local/include VORBIS_LIBS="-L$PREFIX/lib -lvorbis" THEORA=-I$PREFIX/local/include THEORA="-L$PREFIX/lib -ltheora" FISHSOUND=-I$PREFIX/local/include FISHSOUND="-L$PREFIX/lib -lfishsound" ./configure --prefix=$PREFIX/local --disable-speex --disable-shared --enable-static
+OGGZ_CFLAGS=-I$PREFIX/local/include OGGZ_LIBS="-L$PREFIX/local/lib -loggz" VORBIS_CFLAGS=-I$PREFIX/local/include VORBIS_LIBS="-L$PREFIX/local/lib -lvorbis" THEORA_CFLAGS=-I$PREFIX/local/include THEORA_LIBS="-L$PREFIX/local/lib -ltheora" FISHSOUND_CFLAGS=-I$PREFIX/local/include FISHSOUND_LIBS="-L$PREFIX/local/lib -lfishsound" ./configure --prefix=$PREFIX/local --disable-speex --disable-shared --enable-static
 make && make install
 cd ../libsydneyaudio
 ./autogen.sh
